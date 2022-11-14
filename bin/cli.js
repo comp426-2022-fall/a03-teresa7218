07583389d3793d97f28cec586e95828f6ea5c652
Teresa <tpan@unc.edu>
2022-11-14 12:15:34 -0500

    debugged

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
-import { roll } from "/lib/roll.js"
+import {roll} from "../lib/roll.js"
 import minimist from 'minimist';
 
 const args = minimist(process.argv.slice(2));
 
 let result = JSON.stringify(roll(args.sides, args.dice, args.rolls));
 console.log(result);
-
